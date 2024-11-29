a1114493f38821f9e95d8c8b84c60da98dac90a9
snigdha-sukun <ssukun@infinitaslearning.com>
2024-11-29 11:19:27 +0100

    my improved solution

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mediaQuery = window.matchMedia("(max-width: 768px)");
 const shareButton = document.getElementById('share-button');
+const shareIcon = document.getElementById('share-icon');
 const shareOptions = document.getElementById('social-media-options');
 const userInfo = document.getElementById('user-info');
 const userDetails = document.getElementById('user-details');
@@ -12,18 +13,24 @@ function handleClick(event) {
         shareOptions.style.opacity = '1';
         // If screen size is smaller than 768px
         if (mediaQuery.matches)  {
+            shareButton.classList.add('show-social-media');
             // userDetails.style.visibility = 'hidden';
             userDetails.style.display = 'none';
             userInfo.style.backgroundColor = 'hsl(217, 19%, 35%)';
+        } else {
+            shareButton.classList.add('tooltip');
         }
     } else {
         shareOptions.style.display = 'none';
         shareOptions.style.opacity = '0';
         // If screen size is smaller than 768px
         if (mediaQuery.matches) {
+            shareButton.classList.remove('show-social-media');
             userDetails.style.display = 'flex';
             // userDetails.style.visibility = 'visible';
             userInfo.style.backgroundColor = 'white';
+        } else {
+            shareButton.classList.remove('tooltip');
         }
     }
 }
